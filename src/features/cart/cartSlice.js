7cafb9b50e84e821874b74ac250f48501d27db48
BothSann <boththann76@gmail.com>
2025-06-01 15:53:38 +0700

    Add DeleteItem component and implement delete functionality in cartSlice

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -61,3 +61,5 @@ export const getTotalCartQuantity = (state) =>
 
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((accu, item) => accu + item.totalPrice, 0);
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
